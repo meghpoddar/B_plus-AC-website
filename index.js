329ae0608a35a9bf069d091d819b0ac069f2f3ec
suryavempala <vempalas@iitbhilai.ac.in>
2021-09-23 09:39:55 +0530

    Up to 22-09-2021 progress

---
+++ index.js
@@ -0,0 +1,38 @@
+let downiconelement=document.getElementById("downaboutUsicon");
+let rightaboutUsicon = document.getElementById("rightaboutUsicon");
+let aboutsUsdropdown =document.getElementById("AboutsUs-dropdown");
+let rightPortfolioicon = document.getElementById("rightPortfolioicon");
+let downPortfolioicon = document.getElementById("downPortfolioicon");
+let portfoliodropdown=document.getElementById("portfolio-dropdown");
+downiconelement.addEventListener('click',function(event){
+  downiconelement.classList.add("d-none");
+  rightaboutUsicon.classList.remove("d-none");
+  aboutsUsdropdown.classList.remove("d-none");
+  portfoliodropdown.classList.add("d-none");
+   rightPortfolioicon.classList.add("d-none");
+    downPortfolioicon.classList.remove("d-none");
+})
+rightaboutUsicon.addEventListener('click',function(event){
+  downiconelement.classList.remove("d-none");
+  rightaboutUsicon.classList.add("d-none");
+  aboutsUsdropdown.classList.add("d-none");
+  portfoliodropdown.classList.add("d-none");
+   rightPortfolioicon.classList.add("d-none");
+    downPortfolioicon.classList.remove("d-none");
+})
+ downPortfolioicon.addEventListener('click',function(event){
+   downPortfolioicon.classList.add("d-none");
+   rightPortfolioicon.classList.remove("d-none");
+  portfoliodropdown.classList.remove("d-none");
+  aboutsUsdropdown.classList.add("d-none");
+  rightaboutUsicon.classList.add("d-none");
+  downiconelement.classList.remove("d-none")
+ })
+ rightPortfolioicon.addEventListener('click',function(event){
+   downPortfolioicon.classList.remove("d-none");
+   rightPortfolioicon.classList.add("d-none");
+  portfoliodropdown.classList.add("d-none");
+  aboutsUsdropdown.classList.add("d-none");
+  rightaboutUsicon.classList.add("d-none");
+  downiconelement.classList.remove("d-none")
+ })
