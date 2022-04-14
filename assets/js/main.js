b93058435faaa0e64eeb893e585be15e64b28731
Saqlain Shaikh <mdsaqlain4606@gmail.com>
2022-04-14 17:02:36 +0530

    Code Edited4

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,9 +45,9 @@
       let section = select(navbarlink.hash)
       if (!section) return
       if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
+        // navbarlink.classList.add('active')
       } else {
-        navbarlink.classList.remove('active')
+        // navbarlink.classList.remove('active')
       }
     })
   }
